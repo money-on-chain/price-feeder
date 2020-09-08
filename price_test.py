aeb613ebbd4c77a76c7e3b604c2d8c9aece387a8
Juan S. Bokser <juan.bokser@gmail.com>
2020-09-08 15:25:48 -0300

    Some refactor

--- price_test.py
+++ price_test.py
@@ -1,245 +1,49 @@
 import datetime
+from tabulate import tabulate
 
 # local imports
 from price_engines import PriceEngines
-from engines.base import Base as PriceEngineBase
-
-
-class PriceEngineTest1(PriceEngineBase):
-    name = "test_1"
-    description = "Test 1"
-    uri = "http://api.pricefetcher.com/BTCUSD"
-    convert = "BTC_USD"
-
-    def get_price(self, session):
-
-        d_price_info = dict()
-        d_price_info['price'] = 7250.10
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest2(PriceEngineBase):
-    name = "test_2"
-    description = "Test 2"
-    uri = "http://api.pricefetcher.com/BTCUSD"
-    convert = "BTC_USD"
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7258.32
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest3(PriceEngineBase):
-    name = "test_3"
-    description = "Test 3"
-    uri = "http://api.pricefetcher.com/BTCUSD"
-    convert = "BTC_USD"
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7283.81
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest4(PriceEngineBase):
-    name = "test_4"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7286.25
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest5(PriceEngineBase):
-    name = "test_5"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7250.10
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest6(PriceEngineBase):
-    name = "test_6"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7283.81
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest7(PriceEngineBase):
-    name = "test_7"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7286.25
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest8(PriceEngineBase):
-    name = "test_8"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7984.15
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest9(PriceEngineBase):
-    name = "test_9"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7250.1
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest10(PriceEngineBase):
-    name = "test_10"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7283.81
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest11(PriceEngineBase):
-    name = "test_11"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7286.25
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest12(PriceEngineBase):
-    name = "test_12"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-
-        return None, "Error"
-
-
-class PriceEngineTest13(PriceEngineBase):
-    name = "test_13"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 8000
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest14(PriceEngineBase):
-    name = "test_14"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7000
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest15(PriceEngineBase):
-    name = "test_15"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7000
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
-
-
-class PriceEngineTest16(PriceEngineBase):
-    name = "test_16"
-    description = ""
-    uri = ""
-    convert = ""
-
-    def get_price(self, session):
-        d_price_info = dict()
-        d_price_info['price'] = 7000
-        d_price_info['volume'] = 0.0
-        d_price_info['timestamp'] = datetime.datetime.now()
-
-        return d_price_info, None
+from engines.base import Base
+
+def make_fake_engine(name_, price_, error_=False):
+
+    class PriceEngineFake(Base):
+
+        name        = name_
+        description = name_
+        uri         = "http://api.fake-uri.com/BTCUSD"
+        convert     = "BTC_USD"
+
+        def get_price(self, session):
+
+            d_price_info = dict()
+            d_price_info['price'] = price_
+            d_price_info['volume'] = 0.0
+            d_price_info['timestamp'] = datetime.datetime.now()
+
+            if error_:
+                return None, "Error"
+            return d_price_info, None
+
+    return PriceEngineFake
+
+
+PriceEngineTest1  = make_fake_engine("test_1",  7250.10)
+PriceEngineTest2  = make_fake_engine("test_2",  7258.32)
+PriceEngineTest3  = make_fake_engine("test_3",  7283.81)
+PriceEngineTest4  = make_fake_engine("test_4",  7286.25)
+PriceEngineTest5  = make_fake_engine("test_5",  7250.10)
+PriceEngineTest6  = make_fake_engine("test_6",  7283.81)
+PriceEngineTest7  = make_fake_engine("test_7",  7286.25)
+PriceEngineTest8  = make_fake_engine("test_8",  7984.15)
+PriceEngineTest9  = make_fake_engine("test_9",  7250.10)
+PriceEngineTest10 = make_fake_engine("test_10", 7283.81)
+PriceEngineTest11 = make_fake_engine("test_11", 7286.25)
+PriceEngineTest12 = make_fake_engine("test_12", 7286.25, error_=True)
+PriceEngineTest13 = make_fake_engine("test_13", 8000)
+PriceEngineTest14 = make_fake_engine("test_14", 7000)
+PriceEngineTest15 = make_fake_engine("test_15", 7000)
+PriceEngineTest16 = make_fake_engine("test_16", 7000)
 
 
 engines_names_1 = {
@@ -270,6 +74,29 @@ engines_names_4 = {
     "test_16": PriceEngineTest16
 }
 
+def test(price_options, engines_names):
+
+    pr_engine = PriceEngines(price_options, engines_names=engines_names)
+
+    we_prices = pr_engine.get_weighted()
+    we_median = pr_engine.get_weighted_median(we_prices)
+    
+    display_table = []
+    for we_price in we_prices:
+        row = []
+        row.append(we_price['name'])
+        row.append(we_price['price'])
+        row.append(we_price['price_ponderation'])
+        row.append(we_price['ponderation'])
+        display_table.append(row)
+
+    titles = ['Name', 'Price', 'Ponderation', 'Original Ponderation']
+
+    print("")
+    print(tabulate(display_table, headers=titles, tablefmt="pipe"))
+    print("")
+    print("**Weighted median:** {0}".format(we_median ))
+    print("")
 
 if __name__ == '__main__':
 
@@ -280,26 +107,7 @@ if __name__ == '__main__':
         {"name": "test_4", "ponderation": 0.3475, "min_volume":  0.0, "max_delay": 0}
     ]
 
-    pr_engine = PriceEngines(price_options, engines_names=engines_names_1)
-    w_prices = pr_engine.get_weighted()
-    w_median = pr_engine.get_weighted_median(w_prices)
-
-    md_header = '''
-| Name        | Price        | Ponderation    |   Original Ponderation    |             |
-| :--------:  | :----------- | ------------   | -------------------- |-------------------- |
-'''
-    print(md_header)
-
-    for w_price in w_prices:
-        print("| {name} |  {price} | {ponderation} | {o_ponderation} |  |".format(
-            name=w_price['name'],
-            price=w_price['price'],
-            ponderation=format(w_price['price_ponderation'], '.4f'),
-            ponderated=format(w_price['price_ponderated'], '.4f'),
-            o_ponderation=w_price['ponderation']))
-    print("")
-    print("**Weighted median:** {0}".format(w_median))
-    print("")
+    test(price_options, engines_names_1)
 
     price_options = [
         {"name": "test_5", "ponderation": 0.1605, "min_volume": 0.0, "max_delay": 0},
@@ -308,27 +116,8 @@ if __name__ == '__main__':
         {"name": "test_8", "ponderation": 0.2138, "min_volume": 0.0, "max_delay": 0}
     ]
 
-    pr_engine = PriceEngines(price_options, engines_names=engines_names_2)
-    w_prices = pr_engine.get_weighted()
-    w_median = pr_engine.get_weighted_median(w_prices)
-
-    md_header = '''
-| Name        | Price        | Ponderation    |   Original Ponderation    |             |
-| :--------:  | :----------- | ------------   | -------------------- |-------------------- |
-'''
-    print(md_header)
-
-    for w_price in w_prices:
-        print("| {name} |  {price} | {ponderation} | {o_ponderation} |  |".format(
-            name=w_price['name'],
-            price=w_price['price'],
-            ponderation=format(w_price['price_ponderation'], '.4f'),
-            ponderated=format(w_price['price_ponderated'], '.4f'),
-            o_ponderation=w_price['ponderation']))
-    print("")
-    print("**Weighted median:** {0}".format(w_median))
-    print("")
-
+    test(price_options, engines_names_2)
+    
     price_options = [
         {"name": "test_9", "ponderation": 0.1605, "min_volume": 0.0, "max_delay": 0},
         {"name": "test_10", "ponderation": 0.2782, "min_volume": 0.0, "max_delay": 0},
@@ -336,26 +125,7 @@ if __name__ == '__main__':
         {"name": "test_12", "ponderation": 0.2138, "min_volume": 0.0, "max_delay": 0}
     ]
 
-    pr_engine = PriceEngines(price_options, engines_names=engines_names_3)
-    w_prices = pr_engine.get_weighted()
-    w_median = pr_engine.get_weighted_median(w_prices)
-
-    md_header = '''
-| Name        | Price        | Ponderation    |   Original Ponderation    |             |
-| :--------:  | :----------- | ------------   | -------------------- |-------------------- |
-'''
-    print(md_header)
-
-    for w_price in w_prices:
-        print("| {name} |  {price} | {ponderation} | {o_ponderation} |  |".format(
-            name=w_price['name'],
-            price=w_price['price'],
-            ponderation=format(w_price['price_ponderation'], '.4f'),
-            ponderated=format(w_price['price_ponderated'], '.4f'),
-            o_ponderation=w_price['ponderation']))
-    print("")
-    print("**Weighted median:** {0}".format(w_median))
-    print("")
+    test(price_options, engines_names_3)
 
     price_options = [
         {"name": "test_13", "ponderation": 0.2782, "min_volume": 0.0, "max_delay": 0},
@@ -364,23 +134,13 @@ if __name__ == '__main__':
         {"name": "test_16", "ponderation": 0.1605, "min_volume": 0.0, "max_delay": 0}
     ]
 
-    pr_engine = PriceEngines(price_options, engines_names=engines_names_4)
-    w_prices = pr_engine.get_weighted()
-    w_median = pr_engine.get_weighted_median(w_prices)
+    test(price_options, engines_names_4)
 
-    md_header = '''
-| Name        | Price        | Ponderation    |   Original Ponderation    |             |
-| :--------:  | :----------- | ------------   | -------------------- |-------------------- |
-'''
-    print(md_header)
+    price_options = [
+        {"name": "test_13", "ponderation": 1.0, "min_volume": 0.0, "max_delay": 0},
+        {"name": "test_14", "ponderation": 0.25, "min_volume": 0.0, "max_delay": 0},
+        {"name": "test_15", "ponderation": 0.25, "min_volume": 0.0, "max_delay": 0},
+        {"name": "test_16", "ponderation": 0.0, "min_volume": 0.0, "max_delay": 0}
+    ]
 
-    for w_price in w_prices:
-        print("| {name} |  {price} | {ponderation} | {o_ponderation} |  |".format(
-            name=w_price['name'],
-            price=w_price['price'],
-            ponderation=format(w_price['price_ponderation'], '.4f'),
-            ponderated=format(w_price['price_ponderated'], '.4f'),
-            o_ponderation=w_price['ponderation']))
-    print("")
-    print("**Weighted median:** {0}".format(w_median))
-    print("")
+    test(price_options, engines_names_4)
