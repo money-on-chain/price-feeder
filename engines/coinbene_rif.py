5055936b9b48900268f6e5906c12797f6334c375
Juan S. Bokser <juan.bokser@gmail.com>
2020-12-18 15:37:52 -0300

    Implement Coinbene API V2

--- engines/coinbene_rif.py
+++ engines/coinbene_rif.py
@@ -5,14 +5,14 @@ import datetime
 class Engine(Base):
     name        = "coinbene_rif_btc"
     description = "coinbene RIF"
-    uri         = "http://api.coinbene.com/v1/market/ticker?symbol=RIFBTC"
+    uri         = "https://openapi-exchange.coinbene.com/api/exchange/v2/market/ticker/one?symbol=RIF%2FBTC"
     convert     = "RIF_BTC"
 
     @staticmethod
     def map(response_json):
         d_price_info = dict()
-        d_price_info['price'] = float(response_json['ticker'][0]['last'])
-        d_price_info['volume'] = float(response_json['ticker'][0]['24hrVol'])
+        d_price_info['price'] = float(response_json['data']['latestPrice'])
+        d_price_info['volume'] = float(response_json['data']['volume24h'])
         d_price_info['timestamp'] = datetime.datetime.now()
 
         return d_price_info
