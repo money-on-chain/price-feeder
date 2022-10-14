d67178ae3364d11ae4b3747898fbdb0e0a366fad
mulonemartin <mulone.martin@gmail.com>
2022-10-14 15:05:17 -0300

    add Polygon mumbai support

--- add_custom_network.sh
+++ add_custom_network.sh
@@ -1,6 +1,10 @@
 if  [[ $APP_CONNECTION_NETWORK == http* ]] || [[ $APP_CONNECTION_NETWORK == https* ]] ;
 then
+    # Custom network through environment:
+    # Old: export APP_CONNECTION_NETWORK=http://localhost:4444,31
+    # New: export APP_CONNECTION_NETWORK=http://localhost:4444,80001,PolygonMumbai,https://mumbai.polygonscan.com/
+    # Ex.: export APP_CONNECTION_NETWORK=https://rpc-mumbai.matic.today,80001,Polygon,https://mumbai.polygonscan.com/
     echo "Adding custom network ..."
     arrConn=(${APP_CONNECTION_NETWORK//,/ })
-    brownie networks add RskNetwork rskCustomNetwork host=${arrConn[0]} chainid=${arrConn[1]} explorer=https://blockscout.com/rsk/mainnet/api timeout=180
+    brownie networks add ${arrConn[2]}Network ${arrConn[2]}CustomNetwork host=${arrConn[0]} chainid=${arrConn[1]} explorer=${arrConn[3]} timeout=180
 fi
