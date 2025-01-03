48d9e32dc413f1438bb8686fa077d0ae20780a96
Juan S. Bokser <juan.bokser@gmail.com>
2025-01-03 10:06:14 -0300

    fix: bump version

--- price_feeder/tasks.py
+++ price_feeder/tasks.py
@@ -28,7 +28,7 @@ from .contracts import MoCMedianizer, PriceFeed, MoCStateRRC20
 from .base.main import ConnectionHelperBase
 
 
-__VERSION__ = '3.0.3'
+__VERSION__ = '3.0.4'
 
 
 log.info("Starting Price Feeder version {0}".format(__VERSION__))
